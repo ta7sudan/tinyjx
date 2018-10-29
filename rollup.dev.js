0eeda8ced7b868a3114bc39986cd516278bc4867
ta7sudan <ta7sudan@gmail.com>
2018-10-29 18:07:59 +0800

    update rollup.dev.js

--- rollup.dev.js
+++ rollup.dev.js
@@ -2,27 +2,25 @@ import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import { name } from './package.json';
 
-export default [
+export default {
+	input: 'src/index.js',
+	plugins: [
+		replace({
+			DEBUG: JSON.stringify(true)
+		}),
+		babel({
+			exclude: 'node_modules/**'
+		})
+	],
+	treeshake: {
+		propertyReadSideEffects: false
+	},
+	output:
 	{
-		input: 'src/index.js',
-		plugins: [
-			replace({
-				DEBUG: JSON.stringify(true)
-			}),
-			babel({
-				exclude: 'node_modules/**'
-			})
-		],
-		treeshake: {
-			propertyReadSideEffects: false
-		},
-		output:
-		{
-			name,
-			file: 'example/scripts/tinyjx.umd.js',
-			format: 'umd',
-			sourcemap: true,
-			sourcemapPathTransform: () => 'tinyjx.js'
-		}
+		name,
+		file: 'example/scripts/tinyjx.umd.js',
+		format: 'umd',
+		sourcemap: true,
+		sourcemapPathTransform: () => 'tinyjx.js'
 	}
-];
+};
